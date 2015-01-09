1c709112a8c7727767d816722ea01a76145341d0
SongLee24 <lisong.shine@qq.com>
2015-01-09 16:40:13 +0800

    update RBM.java

--- Restricted Boltzmann Machines/RBM.java
+++ Restricted Boltzmann Machines/RBM.java
@@ -11,7 +11,6 @@ import java.util.Random;
 
 public class RBM {
 	
-	private int round;  // the round of training
 	private int n;      // the number of visible units
 	private int m;      // the number of hidden units
 	private double[][] W;       // the weight matrix
@@ -22,8 +21,7 @@ public class RBM {
 	/**
 	 * Constructor
 	 */
-	public RBM(int round, int n, int m, double[][] W, double[] v_bias, double[] h_bias) {
-		this.round = round;
+	public RBM(int n, int m, double[][] W, double[] v_bias, double[] h_bias) {
 		this.n = n;
 		this.m = m;
 		this.W = W;
@@ -35,9 +33,8 @@ public class RBM {
 	/**
 	 * Overloading Constructor
 	 */
-	public RBM(int round, int n, int m)
+	public RBM(int n, int m)
 	{
-		this.round = round;
 		this.n = n;
 		this.m = m;
 		
@@ -94,8 +91,44 @@ public class RBM {
 		
 		for(int i=0; i<n; ++i)
 			v_bias[i] += lr*(v1[i]-sample_v2[i]);
+		
+		System.out.println(getMarginalProbability(v1));
 	}
 	
+	/**
+	 * sum up the marginal probability
+	 */
+	public double getMarginalProbability(int[] v) {
+		double sum = 0.0;
+		for(int j=0; j<m; ++j) {
+			for(int i=0; i<n; ++i) {
+				sum -= v[i]*v_bias[i];
+			}
+		
+			double temp = 0.0;
+			for(int i=0; i<n; ++i)
+				temp -= v[i] * W[i][j];
+			temp -= h_bias[j];
+		
+			sum += Math.log1p(Math.pow(Math.E, temp));
+		}
+		return sum;
+	}
+	
+	
+	private double energy(int[] v, int[] h) {
+		double sum = 0.0;
+		for(int i=0; i<n; ++i) {
+			for(int j=0; j<m; ++j)
+				sum -= v[i]*h[j]*W[i][j];
+			sum -= v[i]*v_bias[i];
+		}
+		
+		for(int j=0; j<m; ++j)
+			sum -= h[j]*h_bias[j];
+		
+		return sum;
+	}
 	
 	/**
 	 * Updating the hidden states Given visible states
@@ -165,32 +198,48 @@ public class RBM {
 	 */
 	public static void main(String[] args) {
 		// training data
-		int[][] train_data = {
-				{1, 1, 1, 0, 0, 0},
-				{1, 0, 1, 0, 0, 0},
-				{1, 1, 1, 0, 0, 0},
-				{0, 0, 1, 1, 1, 0},
-				{0, 0, 1, 0, 1, 0},
-				{0, 0, 1, 1, 1, 0}
-		};
-		
-		double learning_rate = 0.1;
-		int training_epoch = 1000; 
-		int round = 2;
-		int n = 6;
-		int m = 4;
+//		int[][] train_data = {
+//				{1, 1, 1, 0, 0, 0},
+//				{1, 0, 1, 0, 0, 0},
+//				{1, 1, 1, 0, 0, 0},
+//				{0, 0, 1, 1, 1, 0},
+//				{0, 0, 1, 0, 1, 0},
+//				{0, 0, 1, 1, 1, 0}
+//		};
+//		
+//		double learning_rate = 0.1;
+//		int training_epoch = 1000; 
+//		int round = 2;
+//		int n = 6;
+//		int m = 6;
+//		int k = 1;
+//		RBM rbm = new RBM(round, n, m);
+//		
+//		// train
+//		for(int i=0; i<training_epoch; ++i)
+//			//for(int j=0; j<6; ++j)
+//				rbm.cd(train_data[1], learning_rate, k);
+//		
+//		// test data
+//		int[][] test_data = {
+//			{1, 1, 0, 0, 0, 0},
+//			{0, 0, 0, 1, 1, 0}
+//		};
+		int[] v = {1, 0};
+		double[][] W = {{0, 0},{0, 0}};
+		double[] a = {0.5, 0.25};
+		double[] b = {0, 0};
+		int n = 2;
+		int m = 2;
 		int k = 1;
-		RBM rbm = new RBM(round, n, m);
+		RBM rbm = new RBM(n, m, W, a, b);
+		//System.out.println(rbm.getMarginalProbability(v));
+		int[] h1 = new int[m];
+		double[] p = new double[m];
+		rbm.sample_h_given_v(v, h1, p);
 		
-		// train
-		for(int i=0; i<training_epoch; ++i)
-			for(int j=0; j<6; ++j)
-				rbm.cd(train_data[j], learning_rate, k);
-		
-		// test data
-		int[][] test_data = {
-			{1, 1, 0, 0, 0, 0},
-			{0, 0, 0, 1, 1, 0}
-		};
+		for(int i=0; i<m; ++i)
+			System.out.print(p[i]+": " +h1[i]+ " \n");
+		System.out.println();
 	}
 }
